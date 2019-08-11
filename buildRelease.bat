4907f1dea9bd3bc958478a2f2487fd7bb6f2e421
Jonathan Bayer <linuxgurugamer@gmail.com>
2019-08-11 00:00:50 -0400

    reference update

--- buildRelease.bat
+++ buildRelease.bat
@@ -23,7 +23,7 @@ rem    but not always
 rem LICENSE is the license file
 rem README is the readme file
 
-set GAMEDIR=KSP_ColorPicker
+set GAMEDIR=SpaceTuxLibrary
 set GAMEDATA="GameData\"
 set VERSIONFILE=%GAMEDIR%.version
 set LICENSE=License.md
